da9093ea68e75736e38601937cd20e848679438d
Azer444 <Azerrh@code.edu.az>
2022-07-07 23:56:11 +0400

    Celsius and Kelvin

---
+++ Celsius-Kelvin/Celsius-Kelvin/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static Generic.Kelvin;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius celsius = new Celsius(0);
+            Kelvin kelvin = celsius;
+            Console.WriteLine(kelvin.Degree);
+        }
+    }
+    public class Kelvin
+    {
+        public double Degree { get; set; }
+
+        public Kelvin(double degree)
+        {
+            Degree = degree;
+        }
+
+
+
+    }
+    public class Celsius
+    {
+        public double Degree { get; set; }
+
+        public Celsius(double degree)
+        {
+            Degree = degree;
+        }
+        public static implicit operator Kelvin(Celsius celsius)
+        {
+            return new Kelvin(celsius.Degree + 273);
+        }
+    }
+}
